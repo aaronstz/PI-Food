8bdc5ac946602020d427dcd6bc488faf6ca880e5
Aaron <aaronstz@hotmail.com>
2022-06-07 20:09:10 -0300

    Deploy

--- api/src/db.js
+++ api/src/db.js
@@ -13,27 +13,14 @@ const {
 
 let sequelize =
   process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
+    ? new Sequelize(process.env.DATABASE_URL,{
         dialectOptions: {
           ssl: {
             require: true,
             // Ref.: https://github.com/brianc/node-postgres/issues/2009
             rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
+          }
+        }
       })
     : new Sequelize(
         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/foods`,
